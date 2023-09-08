89386d5bd21b7da8d27ed2bf427461c78fbe0f69
Roberto Conrado <robertoconradorc@gmail.com>
2023-09-08 18:47:23 -0300

    async/await

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -49,7 +49,7 @@ exports.getByTag = async(req, res, next) => {
   }
 };
 
-exports.post = (req, res, next) => {
+exports.post = async(req, res, next) => {
   let contract = new ValidationContract();
   contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres.');
   contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres.');
@@ -61,28 +61,38 @@ exports.post = (req, res, next) => {
     return;
   }
 
-  repository.create(req.body)
-    .then(x => {
-      res.status(201).send({ message: 'Produto cadastrado com sucesso!'});
-    }).catch(e => {
-      res.status(400).send({ message: 'Falha ao cadastrar produto!', data: e});
+  try {
+    await repository.create(req.body);
+    res.status(201).send({
+      message: 'Produto cadastrado com sucesso!'
     });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
 };
 
-exports.put = (req, res, next) => {
-  repository.update(req.params.id, req.body)
-  .then(x => {
-      res.status(200).send({ message: 'Produto cadastrado com sucesso!'});
-    }).catch(e => {
-      res.status(400).send({ message: 'Falha ao cadastrar produto!', data: e});
+exports.put = async(req, res, next) => {
+  try {
+    await repository.update(req.params.id, req.body);
+    res.status(200).send({
+      message: 'Produto atualizado com sucesso'
     });
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
+    });
+  }
 };
 
-exports.delete = (req, res, next) => {
-  repository.delete(req.body.id)
-    .then(x => {
+exports.delete = async(req, res, next) => {
+  try {
+    await repository.delete(req.body.id)
       res.status(200).send({ message: 'Produto removido com sucesso!'});
-    }).catch(e => {
-      res.status(400).send({ message: 'Falha ao remover produto!', data: e});
+  } catch (e) {
+    res.status(500).send({
+      message: 'Falha ao processar sua requisição'
     });
+  }  
 };
